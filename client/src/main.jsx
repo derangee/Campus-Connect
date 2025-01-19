1ae1e3dc72df47f303addb9c1bec38935d83d94b
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-19 23:08:40 +0530

    navbar

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,7 @@ import App from './App.jsx'
 import Navbar from './components/navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StrictMode> 
     <Navbar/>
     <App />
   </StrictMode>,

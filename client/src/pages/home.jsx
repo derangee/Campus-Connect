39d11d406d0ce636c5fe525dbd57da62a3b6fcc9
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-23 01:12:09 +0530

    Update home.jsx

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,10 +1,29 @@
+/* eslint-disable react-hooks/rules-of-hooks */
+import { useState, useEffect } from "react";
+import { auth } from "../firebase"; // Import your Firebase config
 
 const home = () => {
-  return (
-    <div>
-      I am home
-    </div>
-  )
-}
+  const [user, setUser] = useState(null);
 
-export default home
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((currentUser) => {
+      setUser(currentUser); // Set the user when authentication state changes
+    });
+
+    return () => unsubscribe(); // Clean up the listener
+  }, []);
+
+  return (
+    <div className="text-center h-screen mt-7">
+      {user ? (
+        <div className="text-center">
+          <h1 className="text-4xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500"> <span className="text-black"> Welcome, </span><span>{user.displayName}</span>! </h1>
+        </div>
+      ) : (
+        <h1 className="text-3xl font-bold">Please Sign-In...</h1>
+      )}
+    </div>
+  );
+};
+
+export default home;

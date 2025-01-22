84f90983ca622d4cbe9a7e77c0462df561ad5936
derangee <daksh_vashishtha@srmap.edu.in>
2025-01-23 01:02:35 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,9 @@
 import { Routes, Route } from "react-router-dom";
 import Request from "./pages/request";
 import Profile from "./pages/Profile"
-import About from "./pages/about";
+import About from "./pages/About";
 import Feedback from "./pages/feedback";
-import Active_request from "./pages/active_request";
+import Active_request from "./pages/Active_request";
 import Home from "./pages/home";
 import React from "react";
 

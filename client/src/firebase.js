766c5b3fb1d698eadc3e1a655280e02a5d2d1ca2
suryanshgour14 <suryansh_gour@srmap.edu.in>
2025-01-24 20:37:57 +0530

    Update firebase.js

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -6,10 +6,10 @@ const firebaseConfig = {
   apiKey: "AIzaSyBmvqM2oO0aBw8N4HJw0TfbUOw8w0WInaM",
   authDomain: "srmap-campus-connect.firebaseapp.com",
   projectId: "srmap-campus-connect",
-  storageBucket: "srmap-campus-connect.appspot.com",
   messagingSenderId: "924558136316",
   appId: "1:924558136316:web:083ce64c33dfaf93235879",
   measurementId: "G-72GM469Q9P",
+  storageBucket: "srmap-campus-connect.appspot.com"
 };
 
 // Initialize Firebase
